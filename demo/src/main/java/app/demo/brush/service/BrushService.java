36ec4424dc8886281a5fb882198a4e035e457076
Meow <liuguozhu720@gmail.com>
2020-06-10 17:00:32 +0800

    JT-116: Add gradle dir Ignore *.iml files Change output way

--- demo/src/main/java/app/demo/brush/service/BrushService.java
+++ demo/src/main/java/app/demo/brush/service/BrushService.java
@@ -1,10 +1,14 @@
 package app.demo.brush.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * @author zoo
  */
 public class BrushService {
+    private final Logger logger = LoggerFactory.getLogger(BrushService.class);
     public void print() {
-        System.out.println("Hello World");
+        logger.info("Hello World");
     }
 }
