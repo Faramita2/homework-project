9d19aac55a8775cab776e0d2779b8dcdbf939875
Meow <liuguozhu720@gmail.com>
2020-06-21 12:35:04 +0800

    JT-112：JT-128：core-ng cd R mode modify table people by modify column `referer_id`

--- db-practice-db-migration/src/main/resources/db/migration/R__people.sql
+++ db-practice-db-migration/src/main/resources/db/migration/R__people.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS `people` (
 		`name` VARCHAR ( 50 ) NOT NULL,
 		`age` INT NOT NULL,
 		`gender` VARCHAR ( 50 ) NOT NULL,
-        `referer_id` INT NOT NULL DEFAULT 0,
+        `referer_id` BIGINT NOT NULL DEFAULT 0,
         `avatar` VARCHAR(255) NOT NULL DEFAULT '',
 		`created_time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
 		`updated_time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -67,3 +67,39 @@ CALL addColumnIfNotExisting('demo', 'people', 'referer_id', 'INT NOT NULL DEFAUL
 
 -- add column `avatar` for table `people`
 CALL addColumnIfNotExisting('demo', 'people', 'avatar', "VARCHAR(255) NOT NULL DEFAULT ''");
+
+DELIMITER $$
+
+DROP PROCEDURE IF EXISTS modifyColumnIfExisting
+$$
+
+CREATE PROCEDURE modifyColumnIfExisting (
+    IN schema_name VARCHAR(100)
+    ,IN table_name VARCHAR(100)
+    , IN column_name VARCHAR(100)
+    , IN column_definition VARCHAR(100)
+)
+BEGIN
+
+    SET @isFieldThere = isColumnExisting(schema_name, table_name, column_name);
+    SELECT @isFieldThere;
+    IF (@isFieldThere <> 0) THEN
+
+        SET @ddl = CONCAT('ALTER TABLE ', CONCAT(CONCAT(schema_name, '.'), table_name));
+        SET @ddl = CONCAT(@ddl, ' ', 'MODIFY COLUMN') ;
+        SET @ddl = CONCAT(@ddl, ' ', column_name);
+        SET @ddl = CONCAT(@ddl, ' ', column_definition);
+
+        PREPARE stmt FROM @ddl;
+        EXECUTE stmt;
+        DEALLOCATE PREPARE stmt;
+
+    END IF;
+
+END;
+$$
+
+DELIMITER ;
+
+-- modify column `referer_id` for table `people`
+CALL modifyColumnIfExisting('demo', 'people', 'referer_id', "BIGINT NOT NULL DEFAULT 0");
